b8d2434f88f7f2200d5f9bd454769ca63675f2a3
Vadym Marukhnenko <vdm.marukhnenko@gmail.com>
2024-02-19 21:30:53 +0000

    fix: small fix

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
